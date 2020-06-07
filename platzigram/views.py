ba36742f5e2ce67376634114c849bc3b97ccd9cc
Ricardo Estrada <ricardo.ares1989@gmail.com>
2020-06-06 19:02:05 -0500

    Implement the sorted function

---
+++ platzigram/views.py
@@ -0,0 +1,31 @@
+"""Platzigram views"""
+
+
+# Django
+from django.http import HttpResponse
+from django.http import JsonResponse
+import json
+# Utilities
+from datetime import datetime
+
+
+def hello_world(request):
+    now = datetime.now().strftime('%b %dth %Y = %H:%M hrs')
+
+    return HttpResponse(f'Oh, hi current server time is {now}')
+
+
+def sorted_numbers(request):
+    """Sorted numbers"""
+    numbers = [int(i) for i in (request.GET['numbers'].split(','))]
+    sorted_ints = sorted(numbers)
+    data = {
+        'status': 'ok',
+        'numbers': sorted_ints,
+        'message': 'Integers sorted succesfully'
+    }
+      
+    return HttpResponse(json.dumps(data, indent=4),
+      content_type='application/json'
+      )
+    # Esto nos devueve un contenido del tipo application json.
