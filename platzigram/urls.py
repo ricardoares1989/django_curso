810fd0539339ef565d400665911cefd897385e82
Ricardo Estrada <ricardo.ares1989@gmail.com>
2020-06-06 20:13:34 -0500

    Create plane html for a view with json

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,16 +1,18 @@
 
 from django.contrib import admin
 from django.urls import path
-from platzigram import views
+from platzigram import views as local_views
+from posts import views as posts_views
 
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello-world/', views.hello_world ),
-    path('sorted/', views.sorted_numbers ),
-    path('hi/<str:name>/<int:age>/', views.say_hi)
+    path('hello-world/', local_views.hello_world ),
+    path('sorted/', local_views.sorted_numbers ),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi),
+    path('posts/', posts_views.list_posts ),
 ]
 
 
---
+++ posts/views.py
@@ -0,0 +1,44 @@
+"""Posts views"""
+
+# Django
+# from django.shortcuts import render
+from django.http import HttpResponse
+
+# utilities
+from datetime import datetime
+
+# Create your views here.
+
+posts = [
+    {
+        'name': 'Mont Blac',
+        'user': 'Yesica Cortes',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://i.picsum.photos/id/1036/200/200.jpg',
+    },
+    {
+        'name': 'Via Láctea',
+        'user': 'C. Vander',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=903',
+    },
+    {
+        'name': 'Nuevo auditorio',
+        'user': 'Thespianartist',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1076',
+    }
+]
+
+def list_posts(requests):
+    """list existing posts."""
+    content = []
+    for post in posts:
+            content.append("""
+            <p><strong>{name}</strong></p>
+            <p><small>{user} - <i>{timestamp}</i></small></p>
+            <figure><img src="{picture}"/></figure>
+            """.format(**post)
+            )
+    return HttpResponse('<br>'.join(content))
+    # Unimos toda nuestra etiqueta en un solo string.
